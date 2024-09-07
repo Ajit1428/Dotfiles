e8012258590f24b7fe0eabdaef18fffbe138e5d7
Ajit Thapa <ajitthapa248@gmail.com>
2024-09-07 18:13:37 +0545

    (update): Updated the transparency in both nvim and hyprland and changed the selection color for nvim in catppuccin theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,18 +3,25 @@ return {
   name = "catppuccin",
   priority = 1000,
   lazy = false,
+  opts = {
+    transparent_background = true,
+  },
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
       term_colors = true,
       highlight_overrides = {
-        mocha = function()
+        mocha = function(colors)
           return {
             Comment = {
               fg = "#00ff7f",
             },
             CursorLine = {
-              bg = "#45475a",
+              bg = colors.overlay0,
+            },
+            Visual = {
+              bg = colors.green,
+              fg = colors.base,
             },
           }
         end,
